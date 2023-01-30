afc86dd453b50e3b36886ebbcaa4a311da18c002
jfmemon <jannatul15-2497@diu.edu.bd>
2023-01-30 19:08:10 +0600

    responsiveness done

--- js/script.js
+++ js/script.js
@@ -25,23 +25,23 @@ const displayLoadedNewsData = loadedNews => {
                       <h5 class="card-title">${news.title}</h5>
                       <p class="card-text">${news.details}</p>
                     </div>
-                    <div class="row gap-3">
-                        <div class="col-md-4">
-                            <div class="row gap-0">
-                                <div class="col-md-3 p-3">
+                    <div class="row gap-2 justify-content-center align-items-center">
+                        <div class="col-lg-4 col-5">
+                            <div class="row">
+                                <div class="col-lg-3 p-3">
                                     <img class="author-image" src="${news.author.img}">
                                 </div>
-                                <div class="col-md-8 px-0 pt-1 align-item-center">
+                                <div class="col-lg-5 px-0 pt-1 align-item-center">
                                     <p class="mb-0">${news.author.name}</p>
                                     <small class="text-secondary">${news.author.published_date}</small>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4 d-flex justify-content-center align-items-center gap-1">
+                        <div class="col-lg-4 col-3 d-flex justify-content-center align-items-center gap-1">
                             <i class="fa-solid fa-eye" style="font-size:15px;"></i>
                             <h6 class="mb-0">${news.total_view} M</h6>
                         </div>
-                        <div class="col-md-3 d-flex justify-content-center align-items-center gap-1">
+                        <div class="col-lg-3 col-3 d-flex justify-content-center align-items-center gap-1">
                             <i class="fa-regular fa-star"></i>
                             <i class="fa-regular fa-star"></i>
                             <i class="fa-regular fa-star"></i>
